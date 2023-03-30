6d036018cd7b8b2c5ac4689f61bd248eead9acbf
Zahra <zahrasiddique026@gmail.com>
2023-03-30 21:39:26 +0200

    Update

--- main.h
+++ main.h
@@ -32,3 +32,13 @@ void ltof(List *L, char *path);
 char *magic_reallocating_fgets(char **bufp, size_t *sizep, FILE *fp);
 
 List *ftol(char *path);
+
+List* listdir ( char* root_dir );
+
+int file_exists(char *file);
+
+void cp(char* to, char* from);
+
+char* hashToPath ( char * hash );
+
+void blobFile ( char* file );
